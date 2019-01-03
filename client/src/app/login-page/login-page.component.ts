16a300e374d110029579d8ad192019ad102c52f2
olekunger <ounger@inbox.ru>
2019-01-03 18:24:55 +0300

    auth

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,40 +1,57 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {AuthService} from "../shared/services/auth.service";
-import {Subscription} from "rxjs";
+import {Component, OnInit, OnDestroy} from '@angular/core'
+import {FormControl, FormGroup, Validators} from "@angular/forms"
+import {AuthService} from "../shared/services/auth.service"
+import {Subscription} from "rxjs"
+import {ActivatedRoute, Params, Router} from "@angular/router"
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+    selector: 'app-login-page',
+    templateUrl: './login-page.component.html',
+    styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit, OnDestroy {
 
-  form: FormGroup
-  aSub: Subscription // чтобы не было утечки памяти при subscribe
+    form: FormGroup
+    aSub: Subscription // чтобы не было утечки памяти при subscribe
 
-  constructor(private auth: AuthService) { }
+    constructor(private auth: AuthService,
+                private router: Router,
+                private route: ActivatedRoute// здесь содержится вся информация о текущем роуте
+    ) {
+    }
 
-  ngOnInit() {
-    this.form = new FormGroup({
-        email: new FormControl(null, [Validators.required, Validators.email]),
-        password: new FormControl(null, [Validators.required, Validators.minLength(6)])
-    })
-  }
+    ngOnInit() {
+        this.form = new FormGroup({
+            email: new FormControl(null, [Validators.required, Validators.email]),
+            password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+        })
 
-  //будет вызываться при переходе на другую страницу
-  ngOnDestroy(){
-    //отписывемся от subscribe, чтобы не было утечек памяти
-     if(this.aSub) this.aSub.unsubscribe()
-  }
+        // подпишемся на объект queryParams текущего роута
+        this.route.queryParams.subscribe((params: Params) => {
+            if (params['registered']) {
+                // теперь вы можете зайти в сиситему используя свои данные
+                console.log('asd')
+            } else if (params['accessDenied']) {
+                // для начала авторизуйтесь
+                console.log('no')
+            }
+        })
+    }
 
-  onSubmit(){
-    this.form.disable()// чтобы не отправлять несколько запросов
+    //будет вызываться при переходе на другую страницу
+    ngOnDestroy() {
+        //отписывемся от subscribe, чтобы не было утечек памяти
+        if (this.aSub) this.aSub.unsubscribe()
+    }
 
-    this.aSub = this.auth.login(this.form.value).subscribe(
-        ()=>console.log('login success'),
-        error=>console.warn('error')
+    onSubmit() {
+        this.form.disable()// чтобы не отправлять несколько запросов
+
+        this.aSub = this.auth.login(this.form.value).subscribe(
+            // ()=>this.router.navigate(['/overview']),
+            () => console.log('success'),
+            error => console.warn('error')
         )
-  }
+    }
 
 }
--- client/src/app/shared/services/auth.service.ts
+++ client/src/app/shared/services/auth.service.ts
@@ -2,12 +2,14 @@ import {Injectable} from '@angular/core'
 import {HttpClient} from "@angular/common/http"
 import {User} from "../interfaces"
 import {Observable} from "rxjs";
+import {tap} from "rxjs/operators";
 
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
+    private token = null
 
     // чтобы делать ajax запросы на сервер инжектируем HttpClient
     constructor(private http: HttpClient) {
@@ -17,8 +19,32 @@ export class AuthService {
     }
 
     // мы ожидаем получить некий объект с токеном
-    login(user: User): Observable<{token: string}> {
-        return this.http.post<{token: string}>('/api/auth/login', user)
+    login(user: User): Observable<{ token: string }> {
+        return this.http.post<{ token: string }>('/api/auth/login', user)
+            .pipe(
+                // сохраним token в переменную для дальнейшего использования при помощи tap
+                tap(({token}) => {
+                    localStorage.setItem('auth-token', token)
+                    this.setToken(token)
+                })
+            )
+    }
 
+    setToken(token: string) {
+        this.token = token
+    }
+
+    getToken(): string {
+        return this.token
+    }
+
+    // находится ли пользователь в сессии
+    isAuthenticated(): boolean {
+        return !!this.token
+    }
+
+    logout (){
+        this.setToken(null)
+        localStorage.clear()
     }
 }
