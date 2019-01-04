7931b7fd5396ee82b0cbec803d3f20c8ec06b61f
olekunger <ounger@inbox.ru>
2019-01-04 21:48:34 +0300

    create update category

--- client/src/app/categories-page/categories-form/categories-form.component.ts
+++ client/src/app/categories-page/categories-form/categories-form.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {CategoriesService} from "../../shared/services/categories.service";
 import {switchMap} from "rxjs/operators";
 import {of} from "rxjs/index";
 import {MaterialService} from "../../shared/classes/material.service";
+import {Category} from "../../shared/interfaces";
 
 @Component({
     selector: 'app-categories-form',
@@ -13,6 +14,11 @@ import {MaterialService} from "../../shared/classes/material.service";
 })
 export class CategoriesFormComponent implements OnInit {
     form: FormGroup
+    @ViewChild('input') inputRef: ElementRef
+    image: File
+    imagePreview = ""
+    category: Category
+
     // режим добавления по умолчанию
     isNew = true
 
@@ -30,7 +36,7 @@ export class CategoriesFormComponent implements OnInit {
         // как только прочитаем params, хотим выполнить еще один асинхронный запрос при помощи switchMap
             .pipe(
                 switchMap(
-                    (params: Params)=>{
+                    (params: Params) => {
                         if (params['id']) {
                             this.isNew = false
                             // возвращаем новый стрим
@@ -43,20 +49,58 @@ export class CategoriesFormComponent implements OnInit {
                 )
             )
             .subscribe(
-                category=>{
-                    if(category) {
+                (category: Category) => {
+                    if (category) {
                         // передаем данные в форму
                         this.form.patchValue({name: category.name})
+                        this.imagePreview = category.imageSrc
+                        this.category = category
                         MaterialService.updateTextInputs()
                     }
                     this.form.enable()
                 },
-                error=>MaterialService.toast(error.error.message)
-                )
+                error => MaterialService.toast(error.error.message)
+            )
+    }
+
+    triggerClick() {
+        this.inputRef.nativeElement.click()
+    }
+
+    onFileUpload(event: any) {
+        const file = event.target.files[0]
+        this.image = file
+
+        const reader = new FileReader()
+
+        reader.onload = () => {
+            this.imagePreview = reader.result
+        }
+
+        reader.readAsDataURL(file)
     }
 
     onSubmit() {
+        let obs$
+        this.form.disable()
+        if(this.isNew){
+            //create
+            obs$ = this.categoriesService.create(this.form.value.name, this.image)
+        } else {
+            obs$ = this.categoriesService.update(this.category._id, this.form.value.name, this.image)
+        }
 
+        obs$.subscribe(
+            category =>{
+                this.form.enable()
+                this.category = category
+                MaterialService.toast('Изменения сохранены')
+            },
+            error=>{
+                MaterialService.toast(error.error.message)
+                this.form.enable()
+            }
+        )
     }
 
 }
