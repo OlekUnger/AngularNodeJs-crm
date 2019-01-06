fe8a623099dada7521dc4e1b3a03cefc3667c254
olekunger <ounger@inbox.ru>
2019-01-06 18:42:42 +0300

    history-list

--- client/src/app/history-page/history-page.component.ts
+++ client/src/app/history-page/history-page.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {MaterialInstance, MaterialService} from "../shared/classes/material.service";
+import {OrdersService} from "../shared/services/orders.service";
+import {Subscription} from "rxjs/index";
+import {Order} from "../shared/interfaces";
+
+const STEP = 2
 
 @Component({
-  selector: 'app-history-page',
-  templateUrl: './history-page.component.html',
-  styleUrls: ['./history-page.component.css']
+    selector: 'app-history-page',
+    templateUrl: './history-page.component.html',
+    styleUrls: ['./history-page.component.css']
 })
-export class HistoryPageComponent implements OnInit {
+export class HistoryPageComponent implements OnInit, OnDestroy, AfterViewInit {
+    @ViewChild('tooltip') tooltipRef: ElementRef
+    isFilterVisible = false
+    tooltip: MaterialInstance
+    oSub: Subscription
+    orders: Order[] = []
+    loadingOrders = false
+    reloading = false
+    offset = 0
+    limit = STEP
+    noMoreOrders = false
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    constructor(private ordersService: OrdersService) {
+    }
+
+    ngOnInit() {
+        this.reloading = true
+        this.fetch()
+    }
+
+    private fetch(){
+        const params = {
+            offset: this.offset,
+            limit: this.limit
+        }
+        this.oSub = this.ordersService.fetch(params).subscribe(
+            orders=>{
+                this.orders = this.orders.concat(orders)
+                this.noMoreOrders = orders.length < STEP
+                this.loadingOrders = false
+                this.reloading = false
+            }
+        )
+    }
+
+
+    loadMore() {
+        this.offset += STEP
+        this.loadingOrders = true
+        this.fetch()
+    }
+
+    ngOnDestroy() {
+        this.tooltip.destroy()
+        if(this.oSub) this.oSub.unsubscribe()
+    }
+
+    ngAfterViewInit() {
+        this.tooltip = MaterialService.initTooltip(this.tooltipRef)
+    }
+
 
 }
+
+
--- client/src/app/shared/services/orders.service.ts
+++ client/src/app/shared/services/orders.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {Order} from "../interfaces";
 import {Observable} from "rxjs/index";
 
@@ -13,5 +13,14 @@ export class OrdersService {
 
     create(order: Order): Observable<Order> {
         return this.http.post<Order>('/api/order', order)
+    }
+
+    fetch(params: any = {}): Observable<Order[]> {
+        return this.http.get<Order[]>('/api/order', {
+            // параметры передаваемые в командную строку
+            params: new HttpParams({
+                fromObject: params
+            })
+        })
     }
 }
